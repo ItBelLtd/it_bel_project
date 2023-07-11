ae356129f0a6756a7878e4c1906ce2e4b7c2bb90
Sergei Demchenko <hb13718@gmail.com>
2023-07-11 12:35:08 +0300

    Add djoser

--- it_bel_project/urls.py
+++ it_bel_project/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("news.urls")),
     path("api/", include("users.urls")),
-    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
