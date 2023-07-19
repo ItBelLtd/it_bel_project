fbdde59157fc743220e3926a06b697690230078d
Serezha_K <60005184+SerezhaK@users.noreply.github.com>
2023-07-20 00:20:47 +0300

    added is_moderator to model user and moderator permission

--- users/views/user.py
+++ users/views/user.py
@@ -2,11 +2,11 @@ from django.http import HttpRequest
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 from ..models.user import User
 from ..serializers.profile import ProfileSerializer
 from ..serializers.users import UserSerializer
 from users.permission import UserOwnerOrReadOnly
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
