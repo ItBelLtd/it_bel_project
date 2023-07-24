5b726e2e636589dc4272fedc248fbe49a19bdce2
Sergei Demchenko <hb13718@gmail.com>
2023-07-24 10:26:28 +0300

    Refactor likes: fix linter

--- news/mixins.py
+++ news/mixins.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 
 from news import services
 
