d631718e3e58ffc46b49a96a7276e47733615fee
Demian <komarovdemian@yandex.ru>
2023-08-21 22:06:17 +0300

    images finally added to news api

--- news/serializers/news.py
+++ news/serializers/news.py
@@ -1,3 +1,4 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from ..models.news import News
@@ -8,8 +9,9 @@ class NewsSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     added = serializers.ReadOnlyField()
     total_likes = serializers.IntegerField(read_only=True)
+    cover = Base64ImageField(required=False)
 
     class Meta:
         model = News
-        fields = ['news_id', 'title', 'author',
+        fields = ['news_id', 'title', 'author', 'cover',
                   'description', 'content', 'total_likes', 'added', ]
