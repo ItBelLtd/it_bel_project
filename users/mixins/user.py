72ed2df533f2c2119070d3bd613058fffc63f837
Sergei Demchenko <hb13718@gmail.com>
2023-08-23 12:19:20 +0300

    Fix linter and constant for users.views.user

--- users/mixins/user.py
+++ users/mixins/user.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import HttpRequest, Request
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from ..models.follow import Follow
