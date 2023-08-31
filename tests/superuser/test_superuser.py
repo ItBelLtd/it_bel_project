3e28ff0cdc9bca1c2ea8eaf281eab9c9a09f12e4
ehmatthes <eric@example.com>
2023-09-01 00:59:42 +0300

    Create tests for superuser

--- tests/superuser/test_superuser.py
+++ tests/superuser/test_superuser.py
@@ -4,38 +4,36 @@ from rest_framework.test import APIClient, APITestCase
 
 from users.models.user import User
 
-
-class UserTestCase(APITestCase):
-    fixtures = ['fixtures/users.json']
-
+class SuperUserTestCase(APITestCase):
+    fixtures = ['fixtures/superuser.json']
     def setUp(self):
         self.user = User.objects.get(user_id=1)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def test_users_get(self):
+    def test_superusers_get(self):
         url = reverse("users-list")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_users_id_get(self):
+    def test_superusers_id_get(self):
         url = reverse("users-detail", args=[self.user.pk])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_users_create_post(self):
+    def test_superusers_create_post(self):
         url = reverse("users-list")
 
         post_data = {
-            "email": "test1@gmail.com",
-            "password": "string123",
-            "username": "test username",
+            "email": "test2@gmail.com",
+            "password": "string1234",
+            "username": "test super_username",
             "author":
                 {
-                    "name": "name",
-                    "surname": "surname",
+                    "name": "super_name",
+                    "surname": "super_surname",
                     "age": 1
                 }
         }
@@ -43,13 +41,13 @@ class UserTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_users_put(self):
+    def test_superusers_put(self):
         url = reverse("users-detail", args=[self.user.pk])
 
         put_data = {
-            "email": "test1@gmail.com",
-            "password": "string123",
-            "username": "test username"
+            "email": "test2@gmail.com",
+            "password": "string1234",
+            "username": "test superusername"
         }
 
         response = self.client.put(url, put_data, format='json')
@@ -60,16 +58,16 @@ class UserTestCase(APITestCase):
         url = reverse("users-detail", args=[self.user.pk])
 
         patch_data = {
-            "email": "test1@gmail.com",
-            "password": "string123",
-            "username": "test username"
+            "email": "test2@gmail.com",
+            "password": "string1234",
+            "username": "test super_username"
         }
 
         response = self.client.patch(url, patch_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_users_delete(self):
+    def test_superusers_delete(self):
         url = reverse("users-detail", args=[self.user.pk])
         response = self.client.delete(url)
 
