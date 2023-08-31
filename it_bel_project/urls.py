b65d5c71ebb9fde223a9058020c79bede50eb87e
Demian <komarovdemian@yandex.ru>
2023-08-31 12:12:11 +0300

    feat+fix: redirect from main page added, tags creation fixed, fixtures fixed, tests fixes, vote added to news serializer

--- it_bel_project/urls.py
+++ it_bel_project/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.views.generic.base import RedirectView
+from django.conf import settings
+
+port = ":8000" if not settings.DOCKER else ""
+docs_url = f'http://127.0.0.1{port}/api/docs/'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=docs_url)),
     path('admin/', admin.site.urls),
     path('api/', include('news.urls')),
     path('api/', include('users.urls')),
--- news/serializers/news.py
+++ news/serializers/news.py
@@ -4,19 +4,65 @@ from rest_framework import serializers
 from ..models.news import News
 from .tag import TagSerializer
 from users.serializers.author import AuthorSerializer
+from ..models.like import Like
+from django.contrib.contenttypes.models import ContentType
+from ..models.tag import Tag
 
 
 class NewsSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     cover = Base64ImageField(required=False)
-    tags = TagSerializer(read_only=True, many=True)
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(), many=True
+    )
+
+    vote = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = News
-        fields = ['news_id', 'title', 'author',
-                  'cover', 'description',
-                  'content', 'tags', 'added',
-                  'total_likes', 'total_dislikes', 'sum_rating']
+        fields = [
+            'news_id',
+            'title',
+            'author',
+            'cover',
+            'description',
+            'content',
+            'tags',
+            'added',
+            'total_likes',
+            'total_dislikes',
+            'sum_rating',
+            'vote'
+        ]
 
-        read_only_fields = ['total_likes', 'total_dislikes',
-                            'sum_rating', 'added']
+        read_only_fields = [
+            'total_likes',
+            'total_dislikes',
+            'sum_rating',
+            'added',
+            'tags',
+            'author',
+            'vote'
+        ]
+
+    def get_vote(self, obj: News) -> int:
+        user = self.context.get('request').user
+        if not user.is_authenticated:
+            return 0
+
+        obj_type = ContentType.objects.get_for_model(obj)
+        like = Like.objects.filter(
+            object_id=obj.pk,
+            content_type=obj_type,
+            user=user
+        ).first()
+        if not like:
+            return 0
+
+        return like.vote
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        news = News.objects.create(**validated_data)
+        news.tags.set(tags)
+        return news
